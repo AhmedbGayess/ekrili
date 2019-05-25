3d799bcbfff705b0231d233e038be39442ad795e
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-05-25 14:03:51 +0100

    initial config

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+require("./db/mongoose");
+const cors = require("cors");
+// const passport = require("passport");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+// require("./config/passport")(passport);
+
+app.use("/images", express.static("./uploads"));
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname + "/../", "client", "build", "index.html")
+  );
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is up on port ${port}`));
