7dd3074cfc1b46acaea36d6c0b846f1cfe7ab455
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-06-10 17:21:41 +0100

    step subtitle

--- client/src/components/ad/CreateAdProgress.js
+++ client/src/components/ad/CreateAdProgress.js
@@ -2,21 +2,28 @@ import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 
-const CreateAdProgress = ({ step }) => (
-  <div className="my-3">
-    <div className="progress-bar">
-      <div
-        className={classNames({
-          "progress-bar__quarter": step === 1,
-          "progress-bar__half": step === 2,
-          "progress-bar__three-quarters": step === 3,
-          "progress-bar__full": step === 4
-        })}
-      />
+const CreateAdProgress = ({ step }) => {
+  let subtitle;
+  if (step == 1) {
+    subtitle = <h3>Qu'avez-vous à louer?</h3>;
+  }
+  return (
+    <div className="my-3">
+      <div className="progress-bar">
+        <div
+          className={classNames({
+            "progress-bar__quarter": step === 1,
+            "progress-bar__half": step === 2,
+            "progress-bar__three-quarters": step === 3,
+            "progress-bar__full": step === 4
+          })}
+        />
+      </div>
+      <h1 className="my-1">Étape {step}</h1>
+      {subtitle}
     </div>
-    <h1>Étape {step}</h1>
-  </div>
-);
+  );
+};
 
 CreateAdProgress.propTypes = {
   step: PropTypes.number.isRequired
