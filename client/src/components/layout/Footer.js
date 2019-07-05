2fafaa204d98490e2e008494b4814dd625dc5d05
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-07-05 18:15:52 +0100

    styling

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,80 +5,78 @@ import PropTypes from "prop-types";
 
 const Footer = ({ isAuthenticated, toggleLoginModal, toggleSignupModal }) => (
   <footer className="footer">
-    <div className="container">
-      <div className="footer__sections">
-        <div className="footer__section">
-          <p className="footer__section__title">À propos</p>
-          <ul className="footer__section__list">
+    <div className="footer__sections">
+      <div className="footer__section">
+        <p className="footer__section__title">À propos</p>
+        <ul className="footer__section__list">
+          <li>
+            <Link to="/" className="footer__section__list__link">
+              Comment ça marche
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="footer__section__list__link">
+              Conditions d'utilisation
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="footer__section__list__link">
+              Qui sommes-nous?
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="footer__section__list__link">
+              Nous contacter
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="footer__section">
+        <p className="footer__section__title">Accès rapide</p>
+        <ul className="footer__section__list">
+          <li>
+            <Link to="/" className="footer__section__list__link">
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/browse-ads/1?sortBy=updatedAt:desc"
+              className="footer__section__list__link"
+            >
+              Toutes les catégories
+            </Link>
+          </li>
+          {!isAuthenticated && (
             <li>
-              <Link to="/" className="footer__section__list__link">
-                Comment ça marche
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="footer__section__list__link">
-                Conditions d'utilisation
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="footer__section__list__link">
-                Qui sommes-nous?
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="footer__section__list__link">
-                Nous contacter
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__section">
-          <p className="footer__section__title">Accès rapide</p>
-          <ul className="footer__section__list">
-            <li>
-              <Link to="/" className="footer__section__list__link">
-                Accueil
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/browse-ads/1?sortBy=updatedAt:desc"
+              <p
                 className="footer__section__list__link"
+                onClick={toggleLoginModal}
               >
-                Toutes les catégories
-              </Link>
+                Se connecter
+              </p>
             </li>
-            {!isAuthenticated && (
-              <li>
-                <p
-                  className="footer__section__list__link"
-                  onClick={toggleLoginModal}
-                >
-                  Se connecter
-                </p>
-              </li>
-            )}
-            {!isAuthenticated && (
-              <li>
-                <p
-                  className="footer__section__list__link"
-                  onClick={toggleSignupModal}
-                >
-                  Inscription
-                </p>
-              </li>
-            )}
-          </ul>
-        </div>
-        <div className="footer__section">
-          <p className="footer__section__title">Contact</p>
-          <a
-            className="footer__section__list__link"
-            href="mailto:contact@ekriha.com"
-          >
-            contact@ekriha.com
-          </a>
-        </div>
+          )}
+          {!isAuthenticated && (
+            <li>
+              <p
+                className="footer__section__list__link"
+                onClick={toggleSignupModal}
+              >
+                Inscription
+              </p>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div className="footer__section">
+        <p className="footer__section__title">Contact</p>
+        <a
+          className="footer__section__list__link"
+          href="mailto:contact@ekriha.com"
+        >
+          contact@ekriha.com
+        </a>
       </div>
     </div>
     <p className="footer__copyright">
