8edd45cb5e06fc0c56fc6508ca07dfd3251dcf04
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-07-07 04:12:04 +0100

    react-snap

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import "./styles/styles.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -7,9 +7,11 @@ import ReactGA from "react-ga";
 
 ReactGA.initialize("UA-143331340-1");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
